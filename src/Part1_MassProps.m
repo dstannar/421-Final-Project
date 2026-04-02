c27247cc4433ae20409f88880b1221a7f2566430
Drew Stannard-Stockton <dstannar@calpoly.edu>
2026-04-02 09:18:54 -0700

    initial commit

---
+++ src/Part1_MassProps.m
@@ -0,0 +1,8 @@
+% Determine the mass and inertial properties of the spacecraft for both the detumble and the normal operations phases based on the MehielSat schematic.
+% 
+% Deliverable
+% For each operational mode (detumble and normal), the Matlab code (.m file and the published .pdf) used to find the spacecraft:
+% 
+% total mass,
+% center of mass relative to the spacecraft bus center of mass in body components,
+% inertia matrix of the whole spacecraft about the center of mass of the spacecraft
