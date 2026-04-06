3b9a4a8a6bfa7c1dcf71576d0c439a975d0f5386
Hailey Orr <horr@calpoly.edu>
2026-04-06 13:09:29 -0700

    added part2

---
+++ src/Part_2_TorqueFreeMotion_Orr.m
@@ -0,0 +1,17 @@
+% Hailey Orr 421 Final Project Part 2
+% torque free motion
+
+
+% givens in normal ops
+
+w_b_eci = [0.001
+    -0.001
+    0.002];
+
+J = [812.0396         0         0
+         0  545.3729         0
+         0         0  627.7083];
+
+m = 640;
+
+com = [0 0 0.2344];
